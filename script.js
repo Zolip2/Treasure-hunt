986fb3f6ec324e453b244774b4d10099b1c9fa5e
Zolip2 <zoltanabraham3@gmail.com>
2021-05-11 17:01:57 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#gomb").click(function(){
+        var str = $("#valasz").val();
+        if (str == 930) {
+            alert("Szia uram")
+        }
+    });
+});
